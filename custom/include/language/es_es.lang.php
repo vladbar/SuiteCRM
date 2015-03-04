21e3f314254a1c47d6e920f9d96cbe76a1325310
Andrew McLauglan <andrew.mclauglan@salesagility.com>
2015-03-04 14:45:49 +0000

    Updated status dropdown language strings.

---
+++ custom/include/language/es_es.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$GLOBALS['app_list_strings']['aok_status_list']=array (
+  'Draft' => 'Draft',
+  'Expired' => 'Expired',
+  'In_Review' => 'In Review',
+  'published_private' => 'Private',
+  'published_public' => 'Public',
+);
